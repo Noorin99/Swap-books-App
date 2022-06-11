d3b2e5ea1ad20fbb1150757ca90200615c5c1a6b
afahgan <afahgan@students.iugaza.edu.ps>
2022-06-11 13:48:36 +0300

    Update Profile Page

---
+++ src/components/Profile/index.js
@@ -0,0 +1,5 @@
+import UserSec from './UserSec';
+import BookTab from './BookTab';
+
+
+export {UserSec,BookTab};
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import BookTab from '../components/Profile/BookTab';
+import UserSec from '../components/Profile/UserSec';
 function Profile() {
-  return <div>Profile</div>;
+  return  <div className='profile_continer'>
+  <UserSec />
+  <BookTab />
+</div>;
 }
 
 export default Profile;
